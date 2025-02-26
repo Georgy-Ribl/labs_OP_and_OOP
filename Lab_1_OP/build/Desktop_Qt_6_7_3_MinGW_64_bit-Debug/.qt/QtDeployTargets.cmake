dfeb74f45e46aa7905bca00a66b01a2813225d1e
Georgy <66615180+Georgy-Ribl@users.noreply.github.com>
2025-02-26 21:54:16 +0300

    Delete Lab_1_OP/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug directory

--- Lab_1_OP/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Lab1_FILE C:/Users/geoko/QtProjects/Lab1/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/Lab1.exe)
-set(__QT_DEPLOY_TARGET_Lab1_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Lab1_RUNTIME_DLLS C:/Qt/6.7.3/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Core.dll)
